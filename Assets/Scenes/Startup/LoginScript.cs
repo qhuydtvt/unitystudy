192bb8d7d5a1be9d52f86e664b7caf677d30f8af
huynq.work <huynq.work@gmail.com>
2019-03-16 16:32:51 +0700

    Hand code FB

--- Assets/Scenes/Startup/LoginScript.cs
+++ Assets/Scenes/Startup/LoginScript.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using Facebook.Unity;
 
 public class LoginScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() => {
+                print("Initialized");
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+                    login();
+                }
+            }
+            );
+        }
+        else
+        {
+            print("Initialized");
+        }
     }
 
-
+    private void login()
+    {
+        var permissions = new List<string>() { "public_profile", "email" };
+        FB.LogInWithReadPermissions(permissions);
+    }
 
     // Update is called once per frame
     void Update()
